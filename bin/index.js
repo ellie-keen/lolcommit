b29f2f0d53296fa0bda9ded77716f4e85c0e0a4a
Ellie Keen <elliekeen95@gmail.com>
2021-05-21 18:39:47 +0100

    Have fall back commit messages if whatthecommit throws an error

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,25 @@
 #! /usr/bin/env node
 const get = require('simple-get')
+
+const offlineCommitMessages = [
+  'another commit',
+  'qwerty',
+  'last one was committed by accident',
+  'ignore last commit',
+  'oh lord, that was a mistake',
+  'bug fix number 260',
+  'ship it',
+  'more changes',
+  'more stuff to fix the previous things',
+  'the product owner made me do it',
+  'add code'
+];
+
+const generateRandomNumber = Math.floor(Math.random() * offlineCommitMessages.length - 1);
  
 get('http://whatthecommit.com/index.txt', function (err, res) {
-  if (err) throw err
+  if (err) {
+    console.log(offlineCommitMessages[generateRandomNumber]);
+  }
   res.pipe(process.stdout)
-})
+});

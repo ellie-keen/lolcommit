b845d7d7209dd32d216578c5e72b23fd43e194ba
Ellie Keen <elliekeen95@gmail.com>
2021-05-21 23:39:30 +0100

    Extract backup messages into its own module

--- bin/index.js
+++ bin/index.js
@@ -1,29 +1,15 @@
 #! /usr/bin/env node
 const got = require('got')
 
-const offlineCommitMessages = [
-  'another commit',
-  'qwerty',
-  'last one was committed by accident',
-  'ignore last commit',
-  'oh lord, that was a mistake',
-  'bug fix number 260',
-  'ship it',
-  'more changes',
-  'more stuff to fix the previous things',
-  'the product owner made me do it',
-  'add code',
-  'aodjpsodDAHHHHH!',
-  'made it DRY, made it DRY'
-];
+const { backupMessages } = require("./backupMessages");
 
-const generateRandomNumber = Math.floor(Math.random() * offlineCommitMessages.length - 1);
+const generateRandomNumber = Math.floor(Math.random() * backupMessages.length - 1);
 
 (async () => {
 	try {
 		const response = await got('http://whatthecommit.com/index.txt');
 		console.log(response.body);
 	} catch (error) {
-		console.log(offlineCommitMessages[generateRandomNumber]);
+		console.log(backupMessages[generateRandomNumber]);
 	}
 })();

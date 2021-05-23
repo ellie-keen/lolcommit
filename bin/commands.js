6f408a7830aac1e2b19bc764477a9bbe8373f374
Jessica Barclay <jessica.barclay@just-eat.com>
2021-05-23 22:14:42 +0100

    return message from function

--- bin/commands.js
+++ bin/commands.js
@@ -2,18 +2,25 @@
 const chalk = require('chalk');
 const got = require('got')
 
-const { backupMessages } = require("./backupMessages");
-const { generateRandomNumber } = require("../utils/generateRandomNumber");
+const { backupMessages } = require('./backupMessages');
+const { generateRandomNumber } = require('../utils/generateRandomNumber');
 
 const getCommitMessage = (prefix) => (async () => {
 	const prefixMessage = chalk.yellowBright('lolcommit: ');
 
 	try {
-		const response = await got('http://whatthecommit.com/index.txt');
-		console.log(prefix ? prefixMessage + response.body : response.body);
+		const response = await got.get('http://whatthecommit.com/index.txt');
+		const message = prefix ? prefixMessage + response.body : response.body
+
+		console.log(message);
+		return message;
 	} catch (error) {
-		console.log(prefix ? prefixMessage + backupMessages[generateRandomNumber(backupMessages.length - 1)] :
-		backupMessages[generateRandomNumber(backupMessages.length - 1)]);
+		const message = prefix ? 
+			prefixMessage + backupMessages[generateRandomNumber(backupMessages.length - 1)] :
+			backupMessages[generateRandomNumber(backupMessages.length - 1)];
+		
+			console.log(message);
+		return message
 	}
 })();
 

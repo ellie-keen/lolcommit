6589d2d6b6395fc507879bc0e65c45a3de204602
jessicabarclay <jessicabarclay.net@gmail.com>
2021-05-23 22:43:12 +0100

    Formatting files

--- bin/backupMessages.js
+++ bin/backupMessages.js
@@ -1,17 +1,17 @@
 const backupMessages = [
-    'another commit',
-    'qwerty',
-    'last one was committed by accident',
-    'ignore last commit',
-    'oh lord, that was a mistake',
-    'bug fix number 260',
-    'ship it',
-    'more changes',
-    'more stuff to fix the previous things',
-    'the product owner made me do it',
-    'add code',
-    'aodjpsodDAHHHHH!',
-    'made it DRY, made it DRY'
-  ];
+  'another commit',
+  'qwerty',
+  'last one was committed by accident',
+  'ignore last commit',
+  'oh lord, that was a mistake',
+  'bug fix number 260',
+  'ship it',
+  'more changes',
+  'more stuff to fix the previous things',
+  'the product owner made me do it',
+  'add code',
+  'aodjpsodDAHHHHH!',
+  'made it DRY, made it DRY'
+];
 
-  module.exports = { backupMessages };
+module.exports = { backupMessages };

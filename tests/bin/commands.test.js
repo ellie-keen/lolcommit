7bd54fe52964a7339bb8562c367fd44934e7a19a
jessicabarclay <jessicabarclay.net@gmail.com>
2021-05-23 22:47:56 +0100

    Add test case for when got.get() throws

--- tests/bin/commands.test.js
+++ tests/bin/commands.test.js
@@ -2,6 +2,9 @@ const got = require('got');
 const commands = require('../../bin/commands');
 
 jest.mock('got');
+jest.mock('../../bin/backupMessages', () => ({
+    'backupMessages':['Random backup message!']
+}));
 
 test('getCommitMessage returns a random commit message', async () => {
     got.get.mockReturnValueOnce({'body': 'Random message!'});
@@ -9,5 +12,16 @@ test('getCommitMessage returns a random commit message', async () => {
 
     responsePromise.then(function(result) {
         expect(result).toBe('Random message!');
+    });
+});
+
+test('getCommitMessage returns a random commit message when GET request fails', async () => {
+    got.get.mockImplementation(() => {
+        throw new Error();
+    });
+    const responsePromise = commands.getCommitMessage();
+
+    responsePromise.then(function(result) {
+        expect(result).toBe('Random backup message!');
     });
 });

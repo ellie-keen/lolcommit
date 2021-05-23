b8135e3be735a8951694d2034c47ceeed4876058
Jessica Barclay <jessica.barclay@just-eat.com>
2021-05-23 22:31:12 +0100

    Add test for when GET request fails

--- tests/bin/commands.test.js
+++ tests/bin/commands.test.js
@@ -2,12 +2,26 @@ const got = require('got');
 const commands = require('../../bin/commands');
 
 jest.mock('got');
+jest.mock('../../bin/backupMessages', () => ({
+    'backupMessages':['Random backup message!']
+}));
 
-test('generateRandomNumber', async () => {
+test('getCommitMessage returns a random commit message', async () => {
     got.get.mockReturnValueOnce({'body': 'Random message!'});
     const responsePromise = commands.getCommitMessage();
 
     responsePromise.then(function(result) {
         expect(result).toBe('Random message!');
-     });
+    });
+});
+
+test('getCommitMessage returns a random commit message when GET request fails', async () => {
+    got.get.mockImplementation(() => {
+        throw new Error();
+    });
+    const responsePromise = commands.getCommitMessage();
+
+    responsePromise.then(function(result) {
+        expect(result).toBe('Random backup message!');
+    });
 });

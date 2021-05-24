133d0cb614c1a8ca0d63cda5aadc384cffb5f9af
Ellie Keen <elliekeen95@gmail.com>
2021-05-24 21:36:49 +0100

    Feed. You. Stuff. No time.

--- tests/utils/generateRandomNumber.test.js
+++ tests/utils/generateRandomNumber.test.js
@@ -1,5 +1,5 @@
 const generateRandomNumber = require('../../utils/generateRandomNumber');
 
 test('generateRandomNumber returns a random value under the maxValue', () => {
-    expect(generateRandomNumber.generateRandomNumber(10)).toBeLessThan(10);
+  expect(generateRandomNumber.generateRandomNumber(10)).toBeLessThan(10);
 });
